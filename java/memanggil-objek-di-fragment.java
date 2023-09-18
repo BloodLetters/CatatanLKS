b0824fd7a044bf9d9de25c4bc0e2e1d959292e74
BloodLetters <103701190+BloodLetters@users.noreply.github.com>
2023-09-18 22:31:03 +0800

    Create memanggil-objek-di-fragment.java

---
+++ java/memanggil-objek-di-fragment.java
@@ -0,0 +1,9 @@
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    // ubah dari return menjadi View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+    View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+    // kode disini
+  
+    return view;
+}
