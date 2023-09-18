5574d16c8099956ae0fdbdc1c2d04655c5f99e41
BloodLetters <103701190+BloodLetters@users.noreply.github.com>
2023-09-18 22:04:00 +0800

    Create Notifikasi-toast.java

---
+++ java/Notifikasi-toast.java
@@ -0,0 +1,5 @@
+// Fragment
+Toast.makeText(getActivity(), "Password/Username Salah!", Toast.LENGTH_SHORT).show();
+
+// Activity
+Toast.makeText(namaClass.this, "Password/Username Salah!", Toast.LENGTH_SHORT).show();
