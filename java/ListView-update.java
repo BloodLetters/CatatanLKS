3b9a89fecd9bcf5c4ffdf330155cf1ac15e0dedf
BloodLetters <103701190+BloodLetters@users.noreply.github.com>
2023-09-20 16:57:10 +0800

    Update ListView-update.java

--- java/ListView-update.java
+++ java/ListView-update.java
@@ -1,4 +1,9 @@
+ListView listview = findViewId(R.id.namaListView);
+
 List<String> data = new ArrayList<>();
+
+// memasukan data
 data.add("test");
+
 ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_expandable_list_item_1, data);
 listView.setAdapter(adapter);
