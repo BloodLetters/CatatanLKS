633122d94ae070270e008e1824166482439892c0
Ashesh <103701190+BloodLetters@users.noreply.github.com>
2024-08-27 17:55:08 +0800

    Create do-while.cpp

---
+++ Cpp(Kuliah)/perulangan/do-while.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int i = 0;
+	do {
+		i++;
+		cout << i << "). Dingin tetapi tidak kejam" << "\n";
+	} while(i < 10);
+}
