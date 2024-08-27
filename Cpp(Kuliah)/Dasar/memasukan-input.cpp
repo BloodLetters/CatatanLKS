980c8e808c73064e4b8266b81116c6ae47bf0e77
Ashesh <103701190+BloodLetters@users.noreply.github.com>
2024-08-27 17:20:59 +0800

    Create memasukan-input.cpp

---
+++ Cpp(Kuliah)/Dasar/memasukan-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	// Deklarasi variable
+	string nama;
+	
+	// Memasukan data ke dalam variable
+	cout << "Nama: ";
+	cin >> nama;
+	
+	// Menampilkan data ke dalam Console
+	cout << "Nama: " << nama;
+}
