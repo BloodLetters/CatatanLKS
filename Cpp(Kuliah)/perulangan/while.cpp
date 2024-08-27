6a166f33be1840fe725e918ea8190249ca8e2bd4
Ashesh <103701190+BloodLetters@users.noreply.github.com>
2024-08-27 17:53:24 +0800

    Create while.cpp

---
+++ Cpp(Kuliah)/perulangan/while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int i = 0;
+	while(true) {
+		i++;
+		
+		cout << i <<"). Kul banget jir \n";
+		
+		if(i == 10) {
+			break;
+		}
+	}
+}
