11348d6cd7499ec300349271e5e68675a281781f
Ashesh <103701190+BloodLetters@users.noreply.github.com>
2024-08-27 17:52:05 +0800

    Create for.cpp

---
+++ Cpp(Kuliah)/perulangan/for.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	// menampilkan text sebanyak 10 kali
+	for(int i=0; i < 11; i++) {
+		cout << i << "). Aku cool \n";
+	}
+}
